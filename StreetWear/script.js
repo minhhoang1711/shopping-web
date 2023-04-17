7055987bc240c2a6d4f8a8376171ddf9a6b718d2
minhhoang1711 <124651779+minhhoang1711@users.noreply.github.com>
2023-04-17 16:20:20 +0700

    push

---
+++ StreetWear/script.js
@@ -0,0 +1,17 @@
+function selectItem(){
+    location.assign("http://127.0.0.1:5500/buy-items-page.html");
+}
+
+function shoppingCart(){
+    location.assign("http://127.0.0.1:5500/shopping-cart.html");
+}
+
+function Buy(){
+    alert("Thank you!");
+    location.assign("http://127.0.0.1:5500/home-page.html");
+}
+
+function chooseSize(){
+    document.getElementById('item-size').style.display = "block";
+} 
+
